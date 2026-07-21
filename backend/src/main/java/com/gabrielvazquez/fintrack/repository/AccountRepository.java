59ee4fc6ba358ee5973909a108c7aaf66a9d7cdd
GabrielVazquez12 <carlosgabrielvazquezvelez>
2026-07-21 08:40:11 -0600

    feat: add jpa repositories for all entities

---
+++ backend/src/main/java/com/gabrielvazquez/fintrack/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielvazquez.fintrack.repository;
+
+import com.gabrielvazquez.fintrack.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
+}
