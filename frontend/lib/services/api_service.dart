32f5bd1e53e22c0d809f4d741deebb78a5a5eba5
GabrielVazquez12 <carlosgabrielvazquezvelez>
2026-07-22 21:27:23 -0600

    fix: use elastic ip instead of dynamic public ip for backend

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -5,7 +5,7 @@ import '../models/transaction.dart';
 
 class ApiService {
   // Your computer's local IP — must be on the same WiFi as the phone.
-  static const String baseUrl = 'http://18.117.111.135:8080/api';
+  static const String baseUrl = 'http://3.129.101.57:8080/api';
 
   final Dio _dio = Dio(BaseOptions(baseUrl: baseUrl));
   String? _token;
